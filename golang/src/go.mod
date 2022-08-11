16544b914f8324292b2cebb145774edbd0a91702
yh <yuukihigaki@hgaiji.local>
2022-08-11 12:05:40 +0900

    add: mainの処理をついか

---
+++ golang/src/go.mod
@@ -0,0 +1,3 @@
+module github.com/Higakinn/trend-batch
+
+go 1.17
--- golang/src/main.go
+++ golang/src/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+		"os"
+
+		. "github.com/Higakinn/trend-batch/trends"
 )
 
 
 func main() {
-	lang := os.Getenv("PROG_LANG")
-	fmt.Println(fmt.Sprintf("%s batch start!!", lang))
-	fmt.Println(fmt.Sprintf("%s batch end!!", lang))
+			// Argsが何件あるか出力します
+		fmt.Println("count:", len(os.Args))
+
+		// Argsの中身を一件ずつ出力します
+		for i, v := range os.Args {
+			fmt.Printf("args[%d] -> %s\n", i, v)
+		}
+    trends := map[string]TrendInterface{
+			"qiita": &Qiita{Name: "qiita"},
+			"zenn": &Zenn{Name: "zenn"},
+    }
+
+    for siteName, trend := range trends {
+			fmt.Println(fmt.Sprintf("%s trend fetching start!\n", siteName))
+			result, err := trend.FetchTrend()
+			if err != nil {
+				// エラー時の処理
+				fmt.Println("error!!")
+			}
+			fmt.Println(result)
+			fmt.Println(fmt.Sprintf("%s trend fetching end!\n", siteName))
+    }
 }
